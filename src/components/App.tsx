66f23dcf016011b6f0bbae9857a94463ba4c8ff8
juanmiguel431 <juanmiguel431@gmail.com>
2023-01-09 23:24:43 -0500

    Transpiling implemented.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import React, { useEffect, useState } from 'react';
 
 function App() {
 
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onClick = () => {
-    console.log(input);
+  useEffect(() => {
+    const startService = async () => {
+      await esbuild.initialize({
+        // wasmURL: './node_modules/esbuild-wasm/esbuild.wasm', //Uncaught (in promise) Error: wasm validation error: at offset 4: failed to match magic number
+        wasmURL: '/esbuild.wasm',
+        worker: true
+      });
+    };
+
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    const result = await esbuild.transform(input, {
+      loader: 'jsx',
+      target: 'es2015'
+    });
+
+    setCode(result.code);
   }
 
   return (
