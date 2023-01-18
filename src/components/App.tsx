c5d2b6548936b496342885b732ef96aeb69d45b0
juanmiguel431 <juanmiguel431@gmail.com>
2023-01-17 22:07:56 -0500

    Defining variables

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -29,7 +29,11 @@ function App() {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()]
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      }
     });
 
     console.log(build);
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -26,7 +26,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-                  const message = require('nested-test-pkg');
+                  const message = require('react');
                   console.log(message);
                 `,
           };
