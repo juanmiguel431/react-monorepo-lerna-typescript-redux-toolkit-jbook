93a59e0ab5fe314a3a2fdaf2cfb57f68b9e154a6
juanmiguel431 <juanmiguel431@gmail.com>
2023-01-17 21:29:09 -0500

    Bundling nested packages

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,12 +6,14 @@ export const unpkgPathPlugin = () => {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
         let path: string;
-        if (args.path === 'tiny-test-pkg') {
-          path = 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js';
-        } else {
+        console.log('onResolve', args);
+        if (args.path === 'index.js') {
           path = args.path;
+        } else if (args.path.includes('./')) {
+          path = new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href;
+        } else {
+          path = `https://unpkg.com/${args.path}`;
         }
 
         return { path: path, namespace: 'a' };
@@ -24,16 +26,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-                  const message = require('tiny-test-pkg');
+                  const message = require('nested-test-pkg');
                   console.log(message);
                 `,
           };
         }
 
-        const { data } = await axios.get<string>(args.path);
+        const { data, request } = await axios.get<string>(args.path);
         return {
           loader: 'jsx',
           contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
         };
       });
     },
