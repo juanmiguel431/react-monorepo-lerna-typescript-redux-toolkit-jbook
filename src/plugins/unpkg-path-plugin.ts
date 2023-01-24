d0357332795cd1e908d5265bf32b81e1081767a1
juanmiguel431 <juanmiguel431@gmail.com>
2023-01-23 23:38:31 -0500

    implementing caching

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,11 +1,17 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import * as localforage from 'localforage';
+import { OnLoadResult } from 'esbuild-wasm';
+
+const fileCache = localforage.createInstance({
+  name: 'filecache'
+});
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
+      build.onResolve({ filter: /.*/ }, async (args) => {
         let path: string;
         if (args.path === 'index.js') {
           path = args.path;
@@ -18,23 +24,34 @@ export const unpkgPathPlugin = () => {
         return { path: path, namespace: 'a' };
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
+      build.onLoad({ filter: /.*/ }, async (args) => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-                  const message = require('react');
+                  const message = require('react-select');
                   console.log(message);
                 `,
           };
         }
 
+        const cachedResult: OnLoadResult | null = await fileCache.getItem(args.path);
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get<string>(args.path);
-        return {
+
+        const result: OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
