ac7974ccd132f66017143691fc7c8f196b8319cc
juanmiguel431 <juanmiguel431@gmail.com>
2023-02-20 23:05:51 -0400

    Applying filters and extracting common caching logic.

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -11,43 +11,52 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
+      build.onLoad({ filter: /^index.js$/ }, (args) => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
 
+      build.onLoad({ filter: /.*/ }, async (args) => {
         const cachedResult: OnLoadResult | null = await fileCache.getItem(args.path);
 
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args) => {
         const { data, request } = await axios.get<string>(args.path);
 
-        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, '\\\'');
 
-        let contents: string;
-        if (fileType === 'css') {
-          const escaped = data
-            .replace(/\n/g, '')
-            .replace(/"/g, '\\"')
-            .replace(/'/g, "\\'");
-
-          contents = `
+        const contents = `
           const style = document.createElement('style');
           style.innerText = '${escaped}';
           document.head.appendChild(style);
         `;
-        } else {
-          contents = data;
-        }
 
         const result: OnLoadResult = {
           loader: 'jsx',
           contents,
+          resolveDir: new URL('./', request.responseURL).pathname
+        };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args) => {
+        const { data, request } = await axios.get<string>(args.path);
+
+        const result: OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         };
 
