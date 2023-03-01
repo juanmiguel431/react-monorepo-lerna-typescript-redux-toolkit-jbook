7069c16d55ba97336b4ff21d0010d76953e0f727
juanmiguel431 <juanmiguel431@gmail.com>
2023-03-01 00:07:29 -0400

    Formatting the code with prettier.

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,10 @@
 import { default as MonacoEditor, OnMount } from '@monaco-editor/react';
 import React from 'react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import { useRef } from 'react';
+import { editor } from 'monaco-editor';
+import IStandaloneCodeEditor = editor.IStandaloneCodeEditor;
 
 interface IProps {
   initialValue?: string;
@@ -9,7 +14,10 @@ interface IProps {
 
 const CodeEditor: React.FC<IProps> = ({ initialValue, onChange }) => {
 
+  const editorRef = useRef<IStandaloneCodeEditor | null>(null);
+
   const onEditorMount: OnMount = (editor, monaco) => {
+    editorRef.current = editor;
     editor.onDidChangeModelContent(() => {
       onChange && onChange(editor.getValue());
     });
@@ -17,7 +25,30 @@ const CodeEditor: React.FC<IProps> = ({ initialValue, onChange }) => {
     // editor.getModel()?.updateOptions({ tabSize: 2 });
   }
 
+  function onFormatClick() {
+    // get the current value of the editor
+    const unformatted = editorRef.current?.getValue();
+
+    if (unformatted === undefined) {
+      return;
+    }
+
+    // format that value
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true
+    });
+
+    // set the formatted valued back in the editor
+    editorRef.current?.setValue(formatted);
+  }
+
   return (
+    <div className="code-editor">
+      <button onClick={onFormatClick}>Format</button>
     <MonacoEditor
       value={initialValue}
       onMount={onEditorMount}
@@ -36,6 +67,7 @@ const CodeEditor: React.FC<IProps> = ({ initialValue, onChange }) => {
         tabSize: 2
       }}
     />
+    </div>
   );
 }
 
