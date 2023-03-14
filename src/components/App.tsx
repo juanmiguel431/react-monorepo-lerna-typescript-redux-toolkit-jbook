aece2b72d9d81008edc2badfb793fd5012429327
juanmiguel431 <juanmiguel431@gmail.com>
2023-03-13 23:10:26 -0400

    Code cell component added.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,28 +1,10 @@
-import React, { useState } from 'react';
-import CodeEditor from './code-editor';
-import Preview from './preview';
-import bundle from '../bundler';
+import React from 'react';
+import CodeCell from './CodeCell';
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    try {
-      setLoading(true);
-      const output = await bundle(input);
-      setCode(output);
-    } finally {
-      setLoading(false);
-    }
-  }
-
   return (
     <div className="app">
-      <CodeEditor initialValue="const a = 1;" onChange={setInput}/>
-      <button onClick={onClick} disabled={loading}>Submit</button>
-      <Preview code={code}/>
+      <CodeCell />
     </div>
   );
 }
---
+++ src/components/CodeCell.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import bundle from '../bundler';
+
+export const CodeCell: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = async () => {
+    try {
+      setLoading(true);
+      const output = await bundle(input);
+      setCode(output);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="app">
+      <CodeEditor initialValue="const a = 1;" onChange={setInput}/>
+      <button onClick={onClick} disabled={loading}>Submit</button>
+      <Preview code={code}/>
+    </div>
+  );
+}
+
+export default CodeCell;
