ce13d394ad036036650f0bc0ae75ae663279234d
juanmiguel431 <juanmiguel431@gmail.com>
2023-03-13 22:56:32 -0400

    plugin folder is now in bundler folder.

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -1,6 +1,6 @@
 import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
-import { fetchPlugin } from '../plugins/fetch-plugin';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 let isInitialized = false;
 
