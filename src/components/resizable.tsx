d47c7ed519da5f739e2623f5a410ffd1699a0e65
juanmiguel431 <juanmiguel431@gmail.com>
2023-03-26 16:47:18 -0400

    Resizable - Consider when the windows gets resized.

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ResizableBox } from 'react-resizable';
 import './resizable.css';
 
@@ -7,11 +7,29 @@ interface ResizableProps {
   children?: React.ReactNode;
 }
 
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return { width, height, };
+};
+
 export const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    const onResize = () => {
+      setWindowDimensions(getWindowDimensions());
+    };
+
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
+  }, []);
+
   return (
     <div className="resizable">
       <ResizableBox
         width={Infinity}
+        maxConstraints={[Infinity, windowDimensions.height * 0.9]}
+        minConstraints={[Infinity, 100]}
         height={300}
         resizeHandles={['s']}
       >
