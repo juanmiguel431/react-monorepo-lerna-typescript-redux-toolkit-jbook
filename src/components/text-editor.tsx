a5377bb220d399edd078d72265bab3fa54ad206a
juanmiguel431 <juanmiguel431@gmail.com>
2023-04-24 21:42:24 -0400

    MDEditor.Markdown added.

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,16 +1,43 @@
 import { default as MDEditor } from '@uiw/react-md-editor';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const TextEditor: React.FC = () => {
-  const [value, setValue] = useState<string | undefined>('');
+  const [value, setValue] = useState<string | undefined>('JMPC');
+  const [editing, setEditing] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (ref.current && event.target && ref.current.contains(event.target as Node)) {
+        return;
+      }
+
+      setEditing(false);
+    };
+
+    document.addEventListener('click', listener, { capture: true });
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    }
+  }, []);
+
+  if (editing) {
+    return (
+      <div className="text-editor" ref={ref}>
+        <MDEditor
+          value={value}
+          onChange={(value, event) => {
+            setValue(value);
+          }}
+        />
+      </div>
+    );
+  }
+
   return (
-    <div className="text-editor">
-      <MDEditor
-        value={value}
-        onChange={(value, event) => {
-          setValue(value);
-        }}
-      />
+    <div className="text-editor" onClick={_ => setEditing(true)}>
+      <MDEditor.Markdown source={value}/>
     </div>
   );
 }
