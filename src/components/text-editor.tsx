ef50e92d86cb2ea0069546c2fd515a5d7d2280ba
juanmiguel431 <juanmiguel431@gmail.com>
2023-04-25 19:34:51 -0400

    Style apply to MD-Editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -38,10 +38,12 @@ const TextEditor: React.FC = () => {
 
   return (
     <div
-      className="text-editor"
+      className="text-editor card"
       onClick={_ => setEditing(true)}
     >
-      <MDEditor.Markdown source={value}/>
+      <div className="card-content">
+        <MDEditor.Markdown source={value}/>
+      </div>
     </div>
   );
 }
